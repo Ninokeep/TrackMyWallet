d4447fc644a061a91a3b8f64212c77744d42ea2e
fababy <studev170@gmail.com>
2024-06-28 09:11:39 +0200

    Add pagination

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,35 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-import type { Updater } from '@tanstack/vue-table'
-import { type Ref } from 'vue'
+import type { Updater } from "@tanstack/vue-table";
+import { type Ref } from "vue";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
-  ref.value
-      = typeof updaterOrValue === 'function'
+export function valueUpdater<T extends Updater<any>>(
+  updaterOrValue: T,
+  ref: Ref
+) {
+  ref.value =
+    typeof updaterOrValue === "function"
       ? updaterOrValue(ref.value)
-      : updaterOrValue
+      : updaterOrValue;
+}
+
+export function totalPages(items: number, take: number) {
+  return Math.ceil(items / take);
+}
+
+export function skip(pages: number, take: number) {
+  return (pages - 1) * take;
+}
+
+export function hasNextPage(currentPage: number, pageCount: number) {
+  return currentPage < pageCount;
+}
+
+export function hasPreviousPage(currentPage: number) {
+  return currentPage > 1;
 }
--- server/api/expenditures/index.get.ts
+++ server/api/expenditures/index.get.ts
@@ -1,23 +1,37 @@
-import { PrismaClient, Prisma } from '@prisma/client'
-import {z} from "zod";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import { hasPreviousPage, skip, totalPages, hasNextPage } from "@/lib/utils";
 
 const dataSchema = z.object({
-  userId: z.number().positive(),
+  p: z.coerce.number().positive().optional().default(1),
 });
 
 export default defineEventHandler(async (event) => {
-
-  const result = await getValidatedRouterParams(event, (body) =>
-      dataSchema.safeParse(body)
-  );
-
+  const result = await getValidatedQuery(event, (body) => {
+    return dataSchema.safeParse(body);
+  });
+  if (!result.success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "The query take must be an integer",
+    });
+  }
   const prisma = new PrismaClient();
 
-  const data = await prisma.expenditure.findMany({
-    where: {
-      userId : result.data?.userId
-    }
-  });
+  const [data, totalExpenditures] = await prisma.$transaction([
+    prisma.expenditure.findMany({
+      take: 10,
+      skip: skip(result.data.p, 10),
+    }),
+    prisma.expenditure.count(),
+  ]);
 
-  return data;
+  return {
+    items: data,
+    currentPage: result.data.p,
+    totalItems: totalExpenditures,
+    totalPages: totalPages(totalExpenditures, 10),
+    hasPrevPage: hasPreviousPage(result.data.p),
+    hasNextPage: hasNextPage(result.data.p, totalPages(totalExpenditures, 10)),
+  };
 });
